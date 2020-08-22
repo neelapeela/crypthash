d2ed4cf39cca7cab20ffdfaa27774c393c7f911a
Neel Madhav <51863122+neelapeela@users.noreply.github.com>
2020-08-22 12:10:00 +0530

    new

--- huntapp/urls.py
+++ huntapp/urls.py
@@ -19,4 +19,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('index.html', views.home),
+    path('leaderboard.html', views.leaderboard),
+    path('register.html', views.register),
+    path('dashboard.html', views.dashboard),
 ]
